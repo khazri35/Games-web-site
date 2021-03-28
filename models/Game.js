662caa775ca93fec38f6aef9ed4596e9b544e2ac
wajih <gabtniwajih123@gmail.com>
2021-03-28 16:50:00 +0100

    wajih: ajout d'un attribut

--- models/Game.js
+++ models/Game.js
@@ -1,19 +1,20 @@
-const mongoose = require("mongoose")
-const schema= mongoose.Schema;
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
 
 const gameSchema = new schema({
-    title: {
-        type: String,
-        required: true
-    },
-    type: {
-        type: String,
-        required: true
-    
-    },
-    demo: String,
+  title: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  demo: String,
 
-    price: Number
+  url: String,
+
+  price: Number,
 });
 
-module.exports= Game=mongoose.model("game", gameSchema);
+module.exports = Game = mongoose.model("game", gameSchema);
