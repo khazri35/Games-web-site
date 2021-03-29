c5a0964080084ec0bba33cb074e7cc13590a441a
wajih <gabtniwajih123@gmail.com>
2021-03-29 22:50:07 +0100

    wajih: *add user model *add user controller *add user routers

---
+++ controllers/user.controllers.js
@@ -0,0 +1,81 @@
+// add user
+const adduser = async (req, res) => {
+  try {
+    const newUser = new User({ ...req.body });
+    // handling error for name & email
+    if (!newUser.name || !newUser.email) {
+      res.status(400).send({ message: "name and email are required" });
+      return;
+    }
+    const field = await User.findOne({ name: req.body.name });
+    if (field) {
+      res.status(400).send({ message: "user already exist" });
+      return;
+    }
+    const result = await newUser.save();
+    res.status(200).send({ message: "user added successfully", result });
+  } catch (error) {
+    res.status(500).send({ message: `${error}` });
+  }
+};
+// get all users
+const getusers = async (req, res) => {
+  try {
+    const result = await User.find();
+    res.status(200).send({ response: result, message: "get all users" });
+  } catch (error) {
+    res.status(404).send({ message: "can not get users" });
+  }
+};
+
+// get one user
+const getuser = async (req, res) => {
+  try {
+    const result = await User.findOne({ _id: req.params.id });
+    res.status(200).send({ response: result, message: "get one user" });
+  } catch (error) {
+    res.status(404).send({ message: "can not get user with this id" });
+  }
+};
+
+// delete user
+const deleteuser = async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const result = await User.findOneAndDelete({ _id });
+    // console.log(result)
+    result
+      ? res
+          .status(200)
+          .send({ response: result, message: "this user is deleted" })
+      : res.send({ message: "already deleted" });
+  } catch (error) {
+    res.status(404).send({ message: "can not delete this user" });
+  }
+};
+
+//  update one user
+const updateuser = async (req, res) => {
+  try {
+    const result = await User.updateOne(
+      { _id: req.params.id },
+      { $set: { ...req.body } }
+    );
+    console.log(result);
+    result.nModified
+      ? res
+          .status(200)
+          .send({ response: result, message: "this user is updated" })
+      : res.send({ response: result, message: "user already updated" });
+  } catch (error) {
+    res.status(404).send({ message: `can not update this user /${error}` });
+  }
+};
+
+module.exports = controllers = {
+  adduser,
+  getusers,
+  getuser,
+  deleteuser,
+  updateuser,
+};
--- server.js
+++ server.js
@@ -1,31 +1,29 @@
-
 console.clear();
 // require express
-const express= require('express');
-const morgan= require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 // instance express
 const app = express();
 // require dotenv & config
-require('dotenv').config();
+require("dotenv").config();
 //  port
-const PORT= process.env.PORT;
+const PORT = process.env.PORT;
 
 // import db
-const dbConnect= require("./config/connectDB");
+const dbConnect = require("./config/connectDB");
 // DB connect
 dbConnect();
 
 // middleware morgan
-app.use(morgan('common'));
+app.use(morgan("common"));
 // middleware body parse
 app.use(express.json());
 // route
 app.use("/api/game", require("./routes/games"));
+app.use("/api/user", require("./routes/users"));
 // create server
-app.listen(PORT, error => {
-  error ? console.error('server not run ${error}'):
-    console.log(`server is running at http://localhost:${PORT}`)
-  });
-
-
-
+app.listen(PORT, (error) => {
+  error
+    ? console.error("server not run ${error}")
+    : console.log(`server is running at http://localhost:${PORT}`);
+});
